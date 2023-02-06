0fee4b11fd4b89c6fa5c4b7303fe43f1e1405a5c
meggidy <mimamitchelle@gmail.com>
2023-02-06 10:38:47 +0300

    Completed

--- app.rb
+++ app.rb
@@ -4,7 +4,6 @@
 #     { id: 4, hello: "Learner" }]
 
 # p [1,2,3]
-# puts "Hello World!"
 # print "Pass this test, please."
 # p [1,2,3]
 
@@ -14,10 +13,17 @@
 # # 1 + 1
 # puts add + add
 
-def add num1, num2
+=begin def add num1, num2
     puts num1
     puts num2
     num1 + num2
 end
 
-puts add(1,3) + add(2,2)
+puts add(1,3) + add(2,2) 
+=end
+
+
+puts "Hello World!"
+p [1,2,3]
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+print "Pass this test, please."
